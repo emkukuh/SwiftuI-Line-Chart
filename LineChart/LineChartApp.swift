96263c4a7cb75015096761984afef56e77532c6a
Kuh <emkukuh@gmail.com>
2022-04-18 05:28:51 +0700

    initial

---
+++ LineChart/LineChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineChartApp.swift
+//  LineChart
+//
+//  Created by kukuh on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LineChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
